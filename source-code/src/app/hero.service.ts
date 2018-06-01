9156728718e50afbce6ae029d761518bbd8a052e
hoalqq <hoalqq@runsystem.net>
2018-06-01 11:57:25 +0700

    update - part Services

--- source-code/src/app/hero.service.ts
+++ source-code/src/app/hero.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of} from 'rxjs';//same callback function
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-    getHeroes(): Hero[] {
-        return HEROES;
+    getHeroes(): Observable<Hero[]> {
+        // TODO: send the message _after_ fetching the heroes
+        this.messageService.add('HeroService: fetched heroes');
+        return of(HEROES);//of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
     }
 }
